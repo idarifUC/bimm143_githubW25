c043366da2976f9a0401850ca2b85ff1645a87c1
Ilyas Darif <idarif@ucsd.edu>
2025-02-25 12:15:50 -0800

    class 4

---
+++ class4.0/class4.0.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "class 4: intro to R"
+#' author: "Ilyas Darif"
+#' date: "Jan 16th, 2025"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+
+
+# make this nicer
+plot (x, sin(x), col="blue", type="l", lwd=3)
